faf963f892b4754a26c5a6c4053dc29f902534a9
heinheinhein <57179017+heinheinhein@users.noreply.github.com>
2025-04-05 02:18:38 +0200

    status changer

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,5 +1,6 @@
 import { Listener } from "@sapphire/framework";
 import { Client } from "discord.js";
+import { startStatusChanger } from "../timer.js";
 
 export class ReadyListener extends Listener {
     public constructor(context: Listener.LoaderContext, options: Listener.Options) {
@@ -12,5 +13,6 @@ export class ReadyListener extends Listener {
     public run(client: Client) {
         const { username, id } = client.user!;
         this.container.logger.info(`Successfully logged in as ${username} (${id})`);
+        startStatusChanger(client);
     }
 }
